3519952bdd5a378a43fe7352ae29b702d6bde337
Praptiiiii <99793975+Praptiiiii@users.noreply.github.com>
2024-06-22 18:44:16 +0530

    Added Java Docs

--- src/main/java/com/demo/assignment/magicalArena/Player.java
+++ src/main/java/com/demo/assignment/magicalArena/Player.java
@@ -1,12 +1,22 @@
 package com.demo.assignment.magicalArena;
 
-
+/**
+ * Represents a player with health, strength, and attack attributes.
+ */
 public class Player {
     private final String name;
     private int health;
     private final int strength;
     private final int attack;
 
+    /**
+     * Initializes a player with the given attributes.
+     * 
+     * @param name     the name of the player
+     * @param health   the health of the player
+     * @param strength the strength of the player
+     * @param attack   the attack power of the player
+     */
     public Player(String name, int health, int strength, int attack) {
         this.name = name;
         this.health = health;
@@ -14,28 +24,57 @@ public class Player {
         this.attack = attack;
     }
 
+    /**
+     * Gets the name of the player.
+     * 
+     * @return the name of the player
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the health of the player.
+     * 
+     * @return the health of the player
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * Gets the strength of the player.
+     * 
+     * @return the strength of the player
+     */
     public int getStrength() {
         return strength;
     }
 
+    /**
+     * Gets the attack power of the player.
+     * 
+     * @return the attack power of the player
+     */
     public int getAttack() {
         return attack;
     }
 
+    /**
+     * Checks if the player is still alive.
+     * 
+     * @return true if the player's health is greater than zero, false otherwise
+     */
     public boolean isAlive() {
         return health > 0;
     }
 
+    /**
+     * Reduces the health of the player by the given amount.
+     * 
+     * @param amount the amount of health to reduce
+     */
     public void reduceHealth(int amount) {
         this.health = Math.max(0, this.health - amount);
     }
 }
-
