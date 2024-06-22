891c9f966fbb0fb0cd9ae5dee746f8e02d5f6e48
Praptiiiii <99793975+Praptiiiii@users.noreply.github.com>
2024-06-22 18:43:15 +0530

    Added Java Docs

--- src/main/java/com/demo/assignment/magicalArena/Dice.java
+++ src/main/java/com/demo/assignment/magicalArena/Dice.java
@@ -1,19 +1,30 @@
 package com.demo.assignment.magicalArena;
 
-
 import java.util.Random;
 
+/**
+ * Represents a dice with a specified number of sides.
+ */
 public class Dice {
     private final int sides;
     private final Random random;
 
+    /**
+     * Initializes a dice with the given number of sides.
+     * 
+     * @param sides the number of sides of the dice
+     */
     public Dice(int sides) {
         this.sides = sides;
         this.random = new Random();
     }
 
+    /**
+     * Rolls the dice and returns a random number between 1 and the number of sides.
+     * 
+     * @return the result of the dice roll
+     */
     public int roll() {
         return random.nextInt(sides) + 1;
     }
 }
-
